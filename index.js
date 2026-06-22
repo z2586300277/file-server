137f6dbfa443ffc11bd85d043759c8519280ca23
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-06-22 19:32:59 +0800

    add: camera_video

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const HOST = (window.origin + window.location.pathname).replace(/\/index.html$/,
 
 export default [
     { id: "fox", name: "狐狸", url: HOST + "model/fox.glb", image: HOST + "cover/fox.webp", type: "model" },
-    { id: "arrow", name: "箭头", url: HOST + "images/arrow.png", image: HOST + "images/arrow.png", type: "image" }
+    { id: "arrow", name: "箭头", url: HOST + "images/arrow.png", image: HOST + "images/arrow.png", type: "image" },
+    { id: "camera_video", name: "监控视频", url: HOST + "video/camera_video.mp4", image: '', type: "video" }
 ];
