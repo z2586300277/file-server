e37eef7f135316d0a45ddde0190b1d021ded3867
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-06-22 19:36:22 +0800

    add: bg_music

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ export default [
     { id: "fox", name: "狐狸", url: HOST + "model/fox.glb", image: HOST + "cover/fox.webp", type: "model" },
     { id: "arrow", name: "箭头", url: HOST + "images/arrow.png", image: HOST + "images/arrow.png", type: "image" },
     { id: "camera_video", name: "监控视频", url: HOST + "video/camera_video.mp4", image: '', type: "video" },
-    { id: "hdrSky", name: "天空", url: HOST + "data/hdrSky.hdr", image: HOST + "cover/hdrSky.webp", type: "data" }
+    { id: "hdrSky", name: "天空", url: HOST + "data/hdrSky.hdr", image: HOST + "cover/hdrSky.webp", type: "data" },
+    { id: "bg_music", name: "背景音乐", url: HOST + "audio/bg_music.mp3", image: '', type: "audio" }
 ];
