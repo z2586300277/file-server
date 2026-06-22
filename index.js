105c106be725ff4844f066e504e3d5be5c08482f
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-06-22 18:24:55 +0800

    add: 狐狸

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const HOST = (window.origin + window.location.pathname).replace(/\/index.html$/, '/')
 
-export default []
+export default [
+    { name: "狐狸", url: HOST + "/model/Fox.glb", image: '', type: "model" }
+];
