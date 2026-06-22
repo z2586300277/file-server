6d294869285b47e52da7d90936f64e321860df1b
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-06-22 19:34:41 +0800

    add: hdrSky

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const HOST = (window.origin + window.location.pathname).replace(/\/index.html$/,
 export default [
     { id: "fox", name: "狐狸", url: HOST + "model/fox.glb", image: HOST + "cover/fox.webp", type: "model" },
     { id: "arrow", name: "箭头", url: HOST + "images/arrow.png", image: HOST + "images/arrow.png", type: "image" },
-    { id: "camera_video", name: "监控视频", url: HOST + "video/camera_video.mp4", image: '', type: "video" }
+    { id: "camera_video", name: "监控视频", url: HOST + "video/camera_video.mp4", image: '', type: "video" },
+    { id: "hdrSky", name: "天空", url: HOST + "data/hdrSky.hdr", image: HOST + "cover/hdrSky.webp", type: "data" }
 ];
