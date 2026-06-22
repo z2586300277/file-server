e579ba7be284c2297c2858fcab63b9347df5d078
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-06-22 19:23:44 +0800

    update: fox

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const HOST = (window.origin + window.location.pathname).replace(/\/index.html$/, '/');
 
 export default [
-    { id: "fox", name: "狐狸", url: HOST + "model/fox.glb", image: '', type: "model" }
+    { id: "fox", name: "狐狸", url: HOST + "model/fox.glb", image: HOST + "cover/fox.webp", type: "model" }
 ];
