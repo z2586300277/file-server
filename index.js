3975a5ce815553ab65f0738d138609a3a52290e6
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-06-25 10:47:46 +0800

    add: china_geo_bound

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ export default [
     { id: "elegant", name: "优雅永不过时", url: HOST + "images/elegant.jpg", image: HOST + "images/elegant.jpg", type: "image" },
     { id: "normal", name: "法线贴图", url: HOST + "images/normal.jpg", image: HOST + "images/normal.jpg", type: "image" },
     { id: "snow", name: "雪花", url: HOST + "images/snow.png", image: HOST + "images/snow.png", type: "image" },
-    { id: "china_geo_json", name: "中国geojson", url: HOST + "data/china_geo_json.json", image: HOST + "cover/china_geo_json.webp", type: "data" }
+    { id: "china_geo_json", name: "中国geojson", url: HOST + "data/china_geo_json.json", image: HOST + "cover/china_geo_json.webp", type: "data" },
+    { id: "china_geo_bound", name: "中国边界geojson", url: HOST + "data/china_geo_bound.json", image: HOST + "cover/china_geo_bound.webp", type: "data" }
 ];
