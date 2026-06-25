086bc40911ac74edba71ed8091ed9fc477947f5d
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-06-25 10:49:22 +0800

    add: vrVideo

--- index.js
+++ index.js
@@ -13,5 +13,6 @@ export default [
     { id: "normal", name: "法线贴图", url: HOST + "images/normal.jpg", image: HOST + "images/normal.jpg", type: "image" },
     { id: "snow", name: "雪花", url: HOST + "images/snow.png", image: HOST + "images/snow.png", type: "image" },
     { id: "china_geo_json", name: "中国geojson", url: HOST + "data/china_geo_json.json", image: HOST + "cover/china_geo_json.webp", type: "data" },
-    { id: "china_geo_bound", name: "中国边界geojson", url: HOST + "data/china_geo_bound.json", image: HOST + "cover/china_geo_bound.webp", type: "data" }
+    { id: "china_geo_bound", name: "中国边界geojson", url: HOST + "data/china_geo_bound.json", image: HOST + "cover/china_geo_bound.webp", type: "data" },
+    { id: "vrVideo", name: "vr全景视频", url: HOST + "video/vrVideo.mp4", image: '', type: "video" }
 ];
