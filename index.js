333aebe16f958b63c9030439058752adbc282b16
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-07-03 07:47:46 +0800

    add: map_emissive

--- index.js
+++ index.js
@@ -18,5 +18,6 @@ export default [
     { id: "bjyhhg", name: "北京优悦幻光科技", url: HOST + "file/bjyhhg.pptx", image: '', type: "file" },
     { id: "map_test", name: "木材贴图", url: HOST + "images/map_test.jpg", image: HOST + "images/map_test.jpg", type: "image" },
     { id: "map_roughness", name: "粗糙度贴图", url: HOST + "images/map_roughness.jpg", image: HOST + "images/map_roughness.jpg", type: "image" },
-    { id: "map_metalness", name: "金属度贴图", url: HOST + "images/map_metalness.jpg", image: HOST + "images/map_metalness.jpg", type: "image" }
+    { id: "map_metalness", name: "金属度贴图", url: HOST + "images/map_metalness.jpg", image: HOST + "images/map_metalness.jpg", type: "image" },
+    { id: "map_emissive", name: "自发光贴图", url: HOST + "images/map_emissive.jpg", image: HOST + "images/map_emissive.jpg", type: "image" }
 ];
